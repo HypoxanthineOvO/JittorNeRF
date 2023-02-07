03615b2a79bda8441e1be674ec560b85d933b533
Hypoxanthine <hyx@HypoxanthinedeMacBook-Pro.local>
2023-02-07 15:12:42 +0800

    Add some package

--- jtnerf.py
+++ jtnerf.py
@@ -1,7 +1,10 @@
-#import jittor
+import jittor
 import os,sys,json,time
+import argparse
 
-
+parser = argparse.ArgumentParser(description="Jittor-NeRF")
+parser.add_argument('--config',help = "Name Of Config")                   # 步骤三，后面的help是我的描述
+args = parser.parse_args()   
 
 if __name__ == "__main__":
-    pass
+    config_path = "./configs/"+args.config
